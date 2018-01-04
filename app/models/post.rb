bb4bca309831d3fdc21df09f251095cf912ec4f9
Erin Land <erinland05@gmail.com>
2018-01-04 22:34:20 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :content, presence: true
-  
+
   belongs_to :user
-  has_many :tags
+  has_many :tags, through: :Post_Tags
 end
